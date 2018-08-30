0eae89afe3786dd115c0c8470f49aef038ec9fbf
kwokwilliam <wkwok16@uw.edu>
2018-08-30 15:26:22 -0700

    built

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -3,6 +3,8 @@ export const imports = {
     import(/* webpackPrefetch: true, webpackChunkName: "src-creating-and-editing" */ 'src/CreatingAndEditing.mdx'),
   'src/HTML.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-html" */ 'src/HTML.mdx'),
+  'src/HTMLII.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-htmlii" */ 'src/HTMLII.mdx'),
   'src/Preface.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-preface" */ 'src/Preface.mdx'),
 }
