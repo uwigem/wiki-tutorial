6727fd9c5989fded28cf5fde1b2ed203c436c283
kwokwilliam <wkwok16@uw.edu>
2018-08-30 16:31:03 -0700

    Add more mediawiki stuff

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,10 +1,14 @@
 export const imports = {
   'src/CreatingAndEditing.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-creating-and-editing" */ 'src/CreatingAndEditing.mdx'),
+  'src/CSS.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-css" */ 'src/CSS.mdx'),
   'src/HTML.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-html" */ 'src/HTML.mdx'),
   'src/HTMLII.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-htmlii" */ 'src/HTMLII.mdx'),
   'src/Preface.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-preface" */ 'src/Preface.mdx'),
+  'src/Templating.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-templating" */ 'src/Templating.mdx'),
 }
