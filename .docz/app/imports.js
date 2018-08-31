96d10bc83a19a9da87539776ca1c00e6724c3b86
kwokwilliam <wkwok16@uw.edu>
2018-08-30 21:07:05 -0700

    updated stuff

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,12 +1,14 @@
 export const imports = {
   'src/CreatingAndEditing.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-creating-and-editing" */ 'src/CreatingAndEditing.mdx'),
-  'src/CSS.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-css" */ 'src/CSS.mdx'),
+  'src/CSSFrameworks.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-css-frameworks" */ 'src/CSSFrameworks.mdx'),
   'src/HTML.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-html" */ 'src/HTML.mdx'),
   'src/HTMLII.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-htmlii" */ 'src/HTMLII.mdx'),
+  'src/Override.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-override" */ 'src/Override.mdx'),
   'src/Preface.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-preface" */ 'src/Preface.mdx'),
   'src/Templating.mdx': () =>
